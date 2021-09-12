6b63f36ffe3d896281e9b60aa371f399b1a94b2e
Aditya Yadav <adi.2002.gwl@gmail.com>
2021-09-12 16:44:05 +0530

    post check

--- dataRoute.js
+++ dataRoute.js
@@ -1,57 +1,3 @@
-// const express = require('express');
-// const Data = require('./Data');
-// const router = express.Router();
-
-// router.get('/', async (req, res) => {
-//   try {
-//     const data = await Data.find();
-//     console.log(data);
-//     return res.status(200).json({
-//       success: true,
-//       count: data.length,
-//       data: data,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(404).json(err);
-//   }
-// });
-
-// router.post('/', async (req, res) => {
-//   //   try {
-//   // const { info } = req.body;
-//   // //     // const data = new Data({
-//   // //     //   info,
-//   // //     // });
-//   // //     // await data.save();
-//   // //     var info = req.body.data;
-//   // //     await info.save();
-//   // //     console.log(info);
-//   // //     console.log('dfafasfasfd');
-//   // //   } catch (error) {
-//   // //     return res.status(400).json(error);
-//   // //   }
-//   // const data = new Data({
-//   //   info,
-//   // });
-//   // data.save();
-//   try {
-//     const data = await Data.find();
-//     // console.log(data);
-//     console.log('Data will be printed HERERERERERERERERERerr');
-//     // console.log(req.body);
-//     console.log(req);
-//     return res.status(200).json({
-//       success: true,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(404).json(err);
-//   }
-// });
-
-// module.exports = router;
-
 const express = require('express');
 const Data = require('./Data');
 const router = express.Router();
@@ -74,18 +20,9 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     console.log('post triggered with data : ', req.body);
-    // const sensor1 = req.body.Humidity;
-    // const sensor2 = req.body.Temprature;
-    // const sensor3 = req.body.Smoke;
-    // const sensor4 = req.body.Fire;
-    // const sensor5 = req.body.Sound;
-    // const sensor6 = req.body.Inches;
-    // const sensor7 = req.body.Cm;
-
     const { Humidity, Temprature, Smoke, Fire, Sound, Inches, Cm } = req.body;
 
-    // MONGO DB DATA INPUT HERE
-    const info = new Data({
+    let user = await Data.findOne({
       Humidity,
       Temprature,
       Smoke,
@@ -94,12 +31,24 @@ router.post('/', async (req, res) => {
       Inches,
       Cm,
     });
-    await info.save();
-    return res.status(200).json({
-      success: true,
-      message: 'Info Saved',
-      data: info,
-    });
+    if (user) {
+      const info = new Data({
+        Humidity,
+        Temprature,
+        Smoke,
+        Fire,
+        Sound,
+        Inches,
+        Cm,
+      });
+      await info.save();
+      return res.status(200).json({
+        success: true,
+        message: 'Info Saved',
+        data: info,
+      });
+    }
+    // MONGO DB DATA INPUT HERE
   } catch (err) {
     console.log(err);
     return res.status(503).json(err);
